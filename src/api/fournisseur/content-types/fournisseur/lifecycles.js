811ae944246bb020aefe5f5045c29c07cfa520f7
omran <omran00kallel@gmail.com>
2025-02-04 00:47:57 +0100

    up

--- src/api/fournisseur/content-types/fournisseur/lifecycles.js
+++ src/api/fournisseur/content-types/fournisseur/lifecycles.js
@@ -1,18 +1,31 @@
 module.exports = {
   async afterCreate(event) {
-    console.log("**********************");
-    console.log(event.result);
-    console.log("**********************");
+    console.log("***********************************");
     let user = await strapi.plugins['users-permissions'].services.user.add({
       blocked: false,
-      confirmed: true,
+      confirmed: false,
       username: event.result.username,
       email: event.result.email,
       password: '123456',
       provider: 'local',
       role: await getRoleId('Fournisseur'),
     });
-  }
+  },
+  async afterDelete(event) {
+    const email = event.result.email;
+
+    // Vérifier si l'utilisateur existe avant de le supprimer
+    let existingUser = await strapi.entityService.findMany("plugin::users-permissions.user", {
+      filters: { email:email },
+    });
+
+    if (existingUser.length > 0) {
+      const userId = existingUser[0].id;
+
+      await strapi.entityService.delete("plugin::users-permissions.user", userId);
+      console.log(`Utilisateur supprimé : ${email}`);
+    }
+  },
 }
 
 async function getRoleId(roleName) {
